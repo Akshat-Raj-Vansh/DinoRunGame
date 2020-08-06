f3262ad988f7288b2dced7b729902108e3387ad2
Akshat Raj Vansh <akshat03rajvansh@gmail.com>
2020-08-06 20:12:44 +0530

    Initial commit

---
+++ android/src/com/arv/dinorun/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.arv.dinorun;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.arv.dinorun.DinoRun;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DinoRun(), config);
+	}
+}
